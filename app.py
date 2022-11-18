f78f3d5d4b2ae4d5981901e7c1f0abc49bb3d8a2
eeee_day_ <joonrose0221@gmail.com>
2022-11-18 14:37:57 +0900

    코드~~

--- app.py
+++ app.py
@@ -4,18 +4,29 @@ from dash import Dash, html, dcc, Input, Output, callback, ctx
 import dash
 import plotly.express as px
 import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 import pandas as pd
+import numpy as np
 import json
-import db
+#import db
+import csv
 from datetime import timedelta, date
 import dash_leaflet as dl
 import dash_leaflet.express as dlx
 from dash_extensions.javascript import arrow_function
+import dash_bootstrap_components as dbc
+import dash_core_components as dcc
 
 #--------------------------------------------------------------------#
 #웹 구성
-#구 -> 일주월, 정지선 TOP5, 인도TOP5 (index_page)
-#동 -> 일주월, CCTV위치(마커표시), 
+#page 1 --> 월간 통계 적용
+#index_page 구성
+# 전체 분포 지도, 
+# 사이드바(발생 순위)->top5 plot 그리기, 
+# 발생수(today, total)
+#page 2
+#구 -> 일주월 추이 그래프, 정지선 TOP5, 인도TOP5 
+#동 -> 일주월 추이 그래프, CCTV위치(마커표시), 
 #cctv->일주일단위의 추이표(현재가 젤 뒤에 있는), CCTV에서의 범법행위 개수
 #--------------------------------------------------------------------#
 
@@ -51,6 +62,8 @@ fig.update_layout(title_text="example",title_font_size=20, width=1100)
 #서울시 동 데이터 로드
 dong = json.load(open('./assets/seoul.json',encoding='utf-8'))
 
+#추이 그래프를 위한 임시 데이터
+data1=pd.read_csv('assets\data1.csv')
 
 #2. content
 header = html.Div(
@@ -93,8 +106,59 @@ dropdown = html.Div(
     className="dropdown_bg",
 )
 
+# 추이 그래프 선택 창
+controls = html.Div(
+    children=[
+        dbc.Card(
+            [dbc.FormGroup(
+                    [
+                        #dbc.Label("Options"),
+                        dcc.RadioItems(id="display_figure", 
+                        options=[{'label': 'None', 'value': 'Nope'},
+                                {'label': '일간 통계', 'value': '일간 통계'},
+                                {'label': '주간 통계', 'value': '주간 통계'},
+                                {'label': '월간 통계', 'value': '월간 통계'}
+                            ],
+                        value='Nope',
+                        labelStyle={'display': 'inline-block', 'width': '10em', 'line-height':'0.5em'}
+                        ) 
+                    ], 
+                ),
+                dbc.FormGroup(
+                    [dbc.Label(""),]
+                ),
+            ],
+            body=True,
+            style = {'font-size': 'large'}),],
+            className='controls',
+    )
+    
 
-#3. layout(고정)
+container = html.Div(
+    children=[
+        dbc.Container([
+            html.H1("범법행위 발생 추이"),
+            html.Hr(),
+            dbc.Row([
+                dbc.Col([controls],xs = 4),
+                dbc.Col([
+                    dbc.Row([
+                        dbc.Col(dcc.Graph(id="analystics")),
+                    ])
+                ]),
+            ]),
+            html.Br(),
+            dbc.Row([
+    
+            ]), 
+        ],
+        fluid=True,)
+    ],
+    className='container',
+)
+
+
+#3. *layout(고정)*
 app.layout = html.Div([
 
     header,
@@ -103,16 +167,18 @@ app.layout = html.Div([
     dcc.Location(id='url', refresh=False),
     
     # content will be rendered in this element
-    html.Div(id='page-content')
+    html.Div(id='page-content'),
     #html.Div(id="hidden_div_for_redirect_callback")
 
 ])
 
+#page1 content 
 index_page = html.Div([
     dropdown,
     dcc.Graph(id='graph',figure=fig, className='graph'),
     html.Div(id='index'),
-    print("index")
+    print("index"),
+
 ])
 
 fig1 = go.Figure(go.Scattermapbox())
@@ -121,7 +187,7 @@ fig1 = go.Figure(go.Scattermapbox())
 
 
 #4. function
-#두 번째 페이지 그래프
+#두 번째 페이지 그래프(layout)
 def analytics_page(location):
     print(location)
     
@@ -169,12 +235,11 @@ def analytics_page(location):
                     html.Div(location),
                     dcc.Dropdown(cities,location,id="city-dropdown",style={'width':"50%","float": "left"}),
                     html.H2("Dashboard",style={"float": "left"})],
-                style={
-                     'height':'150px',
-                     'backgroundColor':'#787878'}
-                ),
-        dcc.Graph(id='dong-graph',figure=fig1,style={'width':"50%","float": "left"}),
-        
+                className='analytics_page-content'),
+        dcc.Graph(id='dong-graph',figure=fig1,className='dong_graph',
+                style={'width':"55%","float": "left", 'margin-top':'25px'}),
+        #일주월 추이그래프
+        container,
         #html.Div(id="map",children=[
            #   dcc.Graph(id='diverse-graph',figure=fig2)],style={'width':"50%","float": "right"})
     ])
@@ -226,7 +291,8 @@ def display_page2(href):
     
     for city in df['SIG_KOR_NM']:
         if href=="/"+city:
-            return analytics_page(city)
+            return analytics_page(city),
+    
 
     return  index_page
 
@@ -243,9 +309,54 @@ def A(clickData):
     return
     
 
+#첫 번째 페이지 추이 그래프
+@app.callback(
+    Output("analystics", "figure"),
+    [Input("display_figure", "value"),
 
-    
+    ],
+)
+def make_graph(display_figure):
 
+    # main trace
+    print(display_figure)
+    if 'Nope' in display_figure:
+        fig = go.Figure()
+        fig.update_layout(plot_bgcolor='rgba(0,0,0,0)', paper_bgcolor='rgba(0,0,0,0)',
+                          yaxis = dict(showgrid=False, zeroline=False, tickfont = dict(color = 'rgba(0,0,0,0)')),
+                          xaxis = dict(showgrid=False, zeroline=False, tickfont = dict(color = 'rgba(0,0,0,0)')))
+        return fig
+
+    if '일간 통계' in display_figure:
+        fig = go.Figure(go.Scatter(x=data1['date'], y=data1['case1'],
+                mode='lines+markers', name='횡단보도 주행 위반'))
+        fig.add_traces(go.Scatter(x=data1['date'], y=data1['case2'],
+                mode='lines+markers', name='보행자도로 통행 위반'))
+        fig.update_layout(template='plotly_dark')
+
+    # prediction trace
+    if '주간 통계' in display_figure:
+        fig = go.Figure(go.Scatter(x=data1['date'], y=data1['case1'],
+                mode='lines+markers', name='횡단보도 주행 위반'))
+        fig.add_traces(go.Scatter(x=data1['date'], y=data1['case2'],
+                mode='lines+markers', name='보행자도로 통행 위반'))
+        fig.update_layout(template='seaborn')
+
+    if '월간 통계' in display_figure:
+        fig = go.Figure(go.Scatter(x=data1['date'], y=data1['case1'],
+                mode='lines+markers', name='횡단보도 주행 위반'))
+        fig.add_traces(go.Scatter(x=data1['date'], y=data1['case2'],
+                mode='lines+markers', name='보행자도로 통행 위반'))
+        fig.update_layout(template='plotly_white')
+
+    # Aesthetics
+    fig.update_layout(margin= {'t':50, 'b':0, 'r': 0, 'l': 0, 'pad': 0})
+    #fig.update_layout(hovermode = 'x')
+    #fig.update_layout(showlegend=True, legend=dict(x=1,y=0.85))
+    fig.update_layout(uirevision='constant')
+    fig.update_layout(title='<b>월별 범법행위 발생 추이')
+
+    return(fig)
 
 
 
