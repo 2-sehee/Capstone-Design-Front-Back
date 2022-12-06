c506e99017a9dd0e66f656b46947cf01e842fdbc
sumeen99 <70104259+sumeen99@users.noreply.github.com>
2022-12-06 18:45:04 +0900

    포트변경

--- app.py
+++ app.py
@@ -11,7 +11,6 @@ import dash_leaflet as dl
 import dash_leaflet.express as dlx
 from dash_extensions.javascript import arrow_function
 import dash_bootstrap_components as dbc
-import numpy as np
 import datetime
 
 
@@ -818,4 +817,4 @@ def graph_cctv_layout(value):
 
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server(host='0.0.0.0', port=8050, debug=True)
